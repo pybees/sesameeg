8027034ad2e1d876f19bf58c7bf5c58e98e83b78
gvluria <49314526+gvluria@users.noreply.github.com>
2019-12-16 18:38:40 +0100

    Delete __init__.py

--- sesame/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-# # # WARNING # # #
-# This list must also be updated in doc/_templates/autosummary/class.rst if it
-# is changed here!
-
-from .smc_utils import (compute_neighbours_matrix, compute_neighbours_probability_matrix,
-                        initialize_radius, estimate_q_in, estimate_s_noise, estimate_s_q,
-                        is_evoked, is_forward, sample_from_sphere, woodbury)
-
-from .check import _check_h5_installed
