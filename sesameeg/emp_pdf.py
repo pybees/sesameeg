19781d162dc0b0c9e78c9129ba6c7cf2b5c2cd80
gvluria <49314526+gvluria@users.noreply.github.com>
2021-07-15 16:01:33 +0200

    Fixed ori (#1)
    * Add fixed orientation and update documentation
    
    Co-authored-by: sarasommariva <sommariva@dima.unige.it>

--- sesameeg/emp_pdf.py
+++ sesameeg/emp_pdf.py
@@ -49,10 +49,11 @@ class EmpPdf(object):
     est_locs : :py:class:`~numpy.ndarray` of :py:class:`~int`
         Estimated sources locations
     """
-    def __init__(self, n_parts, n_verts, lam, dip_mom_std=None, hyper_q=False, verbose=False):
+    def __init__(self, n_parts, n_verts, lam, dip_mom_std=None, fixed_ori=False, hyper_q=False, verbose=False):
 
+        self.fixed_ori = fixed_ori
         self.hyper_q = hyper_q
-        self.particles = np.array([Particle(n_verts, lam, dip_mom_std=dip_mom_std,
+        self.particles = np.array([Particle(n_verts, lam, dip_mom_std=dip_mom_std, fixed_ori=self.fixed_ori,
                                             hyper_q=self.hyper_q) for _ in itertools.repeat(None, n_parts)])
         self.logweights = np.array([np.log(1/n_parts) for _
                                     in itertools.repeat(None, n_parts)])
@@ -83,8 +84,8 @@ class EmpPdf(object):
         r_data : :py:class:`~numpy.ndarray` of :py:class:`~float`, shape (n_sens, n_ist)
             The real part of the data; n_sens is the number of sensors and
             n_ist is the number of time-points.
-        lead_field : :py:class:`~numpy.ndarray` of :py:class:`~float`, shape (n_sens x 3*n_verts)
-            The leadfield matrix.
+        lead_field : :py:class:`~numpy.ndarray` of :py:class:`~float`, shape (n_sens x n_comp*n_verts)
+            The leadfield matrix. (n_comp = 1, if fixed orientation, 3, if free orientation)
         neigh : :py:class:`~numpy.ndarray` of :py:class:`~int`
             The neighbours of each point in the brain discretization.
         neigh_p : :py:class:`~numpy.ndarray` of :py:class:`~float`
--- sesameeg/utils/__init__.py
+++ sesameeg/utils/__init__.py
@@ -2,8 +2,10 @@
 # This list must also be updated in doc/_templates/autosummary/class.rst if it
 # is changed here!
 
-from .smc_utils import (compute_neighbours_matrix, compute_neighbours_probability_matrix,
+from .smc_utils import (compute_neighbours_matrix, compute_correlation_distance_matix,
+                        compute_neighbours_probability_matrix,
                         initialize_radius, estimate_noise_std, estimate_dip_mom_std,
-                        gamma_pdf, is_epochs, is_evoked, is_forward, sample_from_sphere, woodbury)
+                        gamma_pdf, is_epochs, is_evoked, is_forward, normalize,
+                        sample_from_sphere, woodbury)  # compute_cosine_distance,
 
 from .check import _check_h5_installed, _check_pickle_installed
